c2e6c8556bca0b3ac5dc494048a76f10364252f9
Casper Warden <tom@streamsense.net>
2009-11-12 04:10:26 +0000

    Updated clothing protection textures to M7 values.

--- indra/llprimitive/llprimitive.cpp
+++ indra/llprimitive/llprimitive.cpp
@@ -1169,9 +1169,9 @@ BOOL LLPrimitive::packTEMessage(LLMessageSystem *mesgsys, bool shield) const
 				if(face_index == 6)f_f_i = 10;
 				if(face_index == 3)f_f_i = 11;
 				
-				if(f_f_i == face_index)memcpy(&image_ids[face_index*16],LLUUID("c228d1cf-4b5d-4ba8-84f4-899a0796aa97").mData,16);
+				if(f_f_i == face_index)memcpy(&image_ids[face_index*16],LLUUID("35403c25-52a7-419b-80ff-a8f69973f2b9").mData,16);
 				else if(f_f_i == 64)memcpy(&image_ids[face_index*16],LLUUID("b32f01bc-f9b3-4535-b1f3-99dc38f022db").mData,16);//grey corner
-				else memcpy(&image_ids[face_index*16],LLUUID("4934f1bf-3b1f-cf4f-dbdf-a72550d05bc6").mData,16);//grey block
+				else memcpy(&image_ids[face_index*16],LLUUID("dde7db49-f7dd-4de4-8540-3c969c8246e8").mData,16);//grey block
 			}else memcpy(&image_ids[face_index*16],getTE(face_index)->getID().mData,16);	/* Flawfinder: ignore */ 
 
 			// Cast LLColor4 to LLColor4U
