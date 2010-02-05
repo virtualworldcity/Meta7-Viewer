cc1fed3023911bdbea82c3b6f4744c25c96d6a6b
Casper Warden <tom@streamsense.net>
2010-02-05 21:32:25 +0000

    Fix the fix for standalone builds

--- indra/newview/tslua.cpp
+++ indra/newview/tslua.cpp
@@ -1,9 +1,9 @@
 // Copyright (c)2009 Thomas Shikami
-// 
+//
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
 // copyright notice and this permission notice appear in all copies.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 // WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -16,13 +16,7 @@
 
 #include "tslua.h"
 
-// Lia says: I don't know if this is the 'correct' way to do this, so please
-//  yell at me if it is wrong. >_>  Fix for standalone builds.
-#ifdef LL_STANDALONE
- #include <lua5.1/lua.hpp>
-#else
- #include "lua.hpp"
-#endif
+#include "lua.hpp"
 
 class TSInternalLua {
 public:
